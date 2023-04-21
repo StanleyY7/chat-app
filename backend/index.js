a51325501433ced674370f3c61507b685b592309
StanleyY7 <stanleyyu7@gmail.com>
2023-04-21 14:21:19 +1000

    added get request

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,20 @@ const axios = require("axios");
 app.use(express.json());
 app.use(cors({ origin: true })); // To avoid cors errors by allowing calls to this server from any origin.
 
+// GET
+
+app.get("/users", async (res) => {
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/", {
+      headers: { "PRIVATE-KEY": "1c959afc-9e19-4da5-b9c7-997e6cebb676" },
+    });
+
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
 // POST
 
 app.post("/auth", async (req, res) => {
