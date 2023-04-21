195ee1693304dc8f7e49a7bf1b5fbe7117189d05
StanleyY7 <stanleyyu7@gmail.com>
2023-04-21 15:53:22 +1000

    frontend deployment

--- chat-frontend/src/Containers/AuthPage/AuthPage.cy.tsx
+++ chat-frontend/src/Containers/AuthPage/AuthPage.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import AuthPage from './AuthPage'
+import React from "react";
+import AuthPage from "./AuthPage";
 
-describe('<AuthPage />', () => {
-  it('renders', () => {
+describe("<AuthPage />", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<AuthPage />)
-  })
-})
+    cy.mount(<AuthPage />);
+  });
+});
